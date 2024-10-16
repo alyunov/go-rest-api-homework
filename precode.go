240abc1ae7359fe4f53c4982334ffeae66292bb9
P. Alyunov <oficaln@yandex.ru>
2024-10-16 23:25:49 +0300

    first commit

--- precode.go
+++ precode.go
@@ -1,13 +1,22 @@
+// Пакет main реализует аналог приложения TODO-LIST со списком задач.
+//
+// Доступный функционал:
+// - вывод списка всех задач (Эндпоинт "/tasks". Метод GET.);
+// - добавление задачи (Эндпоинт "/tasks". Метод POST.);
+// - вывод определенной задачи (Эндпоинт "/tasks/{ID}". Метод GET.);
+// - удаление определенной задачи (Эндпоинт "/tasks/{ID}". Метод DELETE.).
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
-// Task ...
+// Структура Task (задача).
 type Task struct {
 	ID           string   `json:"id"`
 	Description  string   `json:"description"`
@@ -15,6 +24,7 @@ type Task struct {
 	Applications []string `json:"applications"`
 }
 
+// Мапа tasks (задачи).
 var tasks = map[string]Task{
 	"1": {
 		ID:          "1",
@@ -39,14 +49,91 @@ var tasks = map[string]Task{
 	},
 }
 
-// Ниже напишите обработчики для каждого эндпоинта
-// ...
+// getTasks обработчик для получения всех задач.
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(data)
+}
+
+// adddTask обработчик для отправки задачи на сервер.
+func addTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, ok := tasks[task.ID]
+	if ok {
+		http.Error(w, "Задача с таким ID уже существует", http.StatusBadRequest)
+		return
+	}
+
+	tasks[task.ID] = task
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+// getTask обработчик для получения задачи по ID.
+func getTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+
+	data, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(data)
+}
+
+// deleteTask обработчик для удаления задачи по ID.
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	_, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+
+	delete(tasks, id)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
 
-	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getTasks)
+	r.Post("/tasks", addTask)
+	r.Get("/tasks/{id}", getTask)
+	r.Delete("/tasks/{id}", deleteTask)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
